eb1230bfc3e07d3f6b6fd15e79cd39bea18b81f0
Sandeep Hebbar <sandeeph@gmail.com>
2016-01-29 11:23:36 +0530

    db.seed pin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = User.find(1)
-user.update_attribute(:admin, true)
-user.update_attribute(:activated, true)
-user.update_attribute(:activated_at, Time.zone.now)
-user.attributes = {admin: true, activated: true, activated_at: Time.zone.now}
+#user = User.find(1)
+#user.update_attribute(:admin, true)
+#user.update_attribute(:activated, true)
+#user.update_attribute(:activated_at, Time.zone.now)
+#user.attributes = {admin: true, activated: true, activated_at: Time.zone.now}
+pin = Pins.find(1)
+pin.update_attribute(:activated, true)
